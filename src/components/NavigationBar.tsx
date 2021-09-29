be7344fae4ea673418124215be8ffe81401a7bef
NatanTavares <nattantavares.s15@gmail.com>
2021-09-29 17:22:04 -0300

    fix(navigation): Hide routes that make no sense for the user to access when logged in. ex: Login, Register

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,34 +1,72 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react";
+import {
+  Button,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Text,
+} from "@chakra-ui/react";
+import { useMeQuery } from "../generated/graphql";
 import NextLink from "next/link";
 
 type Props = {};
 
 export const NavigationBar: React.FC<Props> = () => {
+  const [{ data }] = useMeQuery();
+
   return (
     <>
-      <Breadcrumb
-        fontSize="md"
-        separator="|"
-        bg="gray.700"
-        p={4}
-        textAlign="end"
-      >
-        <BreadcrumbItem isCurrentPage>
-          <BreadcrumbLink href="/">Home</BreadcrumbLink>
-        </BreadcrumbItem>
+      {!!data?.me?.user ? (
+        <Breadcrumb
+          fontSize="md"
+          separator="|"
+          bg="gray.700"
+          p={2}
+          textAlign="end"
+        >
+          <BreadcrumbItem>
+            <Button variant="ghost" size="sm">
+              {data?.me?.user?.username}
+            </Button>
+            {/* <Text>{data?.me?.user?.username}</Text> */}
+          </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <NextLink href="/login">
-            <BreadcrumbLink>Login</BreadcrumbLink>
-          </NextLink>
-        </BreadcrumbItem>
+          <BreadcrumbItem>
+            <Button colorScheme="teal" variant="ghost" size="sm">
+              Logout
+            </Button>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      ) : (
+        <Breadcrumb
+          fontSize="md"
+          separator="|"
+          bg="gray.700"
+          p={2}
+          textAlign="end"
+        >
+          <BreadcrumbItem>
+            <Button variant="ghost" size="sm">
+              <BreadcrumbLink>Home</BreadcrumbLink>
+            </Button>
+          </BreadcrumbItem>
 
-        <BreadcrumbItem>
-          <NextLink href="/register">
-            <BreadcrumbLink>Register</BreadcrumbLink>
-          </NextLink>
-        </BreadcrumbItem>
-      </Breadcrumb>
+          <BreadcrumbItem>
+            <NextLink href="/login">
+              <Button colorScheme="teal" variant="ghost" size="sm">
+                Login
+              </Button>
+            </NextLink>
+          </BreadcrumbItem>
+
+          <BreadcrumbItem>
+            <NextLink href="/register">
+              <Button colorScheme="teal" variant="ghost" size="sm">
+                Register
+              </Button>
+            </NextLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      )}
     </>
   );
 };
