3ba17b0688ef666ea6f88f5ea8f044ff95d6fd3f
NatanTavares <nattantavares.s15@gmail.com>
2021-09-29 12:59:37 -0300

    feat(register): Implements the register error handling

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,12 +1,15 @@
 import { useRegisterUserMutation } from "../generated/graphql";
 import { InputField } from "../components/InputField";
-import { Wrapper } from "../components/Wrapper";
+import { toErrorMap } from "../utils/toErrorMap";
 import { Button, Stack } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { useRouter } from "next/router";
 import { Formik, Form } from "formik";
 import type { NextPage } from "next";
 import Head from "next/head";
 
 const Register: NextPage = () => {
+  const router = useRouter();
   const [_, register] = useRegisterUserMutation();
 
   return (
@@ -18,13 +21,19 @@ const Register: NextPage = () => {
       <Wrapper variant="small">
         <Formik
           initialValues={{ username: "", password: "" }}
-          onSubmit={async ({ username, password }) => {
+          onSubmit={async ({ username, password }, { setErrors }) => {
             const registerUserCredentials = {
               username,
               password,
             };
 
             const { data } = await register({ registerUserCredentials });
+
+            if (!!data?.registerUser.errors) {
+              setErrors(toErrorMap(data.registerUser.errors));
+            } else if (data?.registerUser.user) {
+              router.push("/");
+            }
           }}
         >
           {({ isSubmitting }) => (
